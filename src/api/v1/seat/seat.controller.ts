6ccd777dccac22b9054f3d8e851f5bbc393f6ccb
Ho Van Quoc Huy <huyhvq@kdhm-solutions.com>
2024-07-17 16:46:24 +0700

    feat: API Create many seat (at the same time)

--- src/api/v1/seat/seat.controller.ts
+++ src/api/v1/seat/seat.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { SeatService } from './seat.service';
 import { CreateSeatDto } from './dto/create-seat.dto';
+import { CreateManySeatsDto } from './dto/create-many-seats.dto';
 
 @Controller('seat')
 export class SeatController {
@@ -20,6 +21,13 @@ export class SeatController {
     @Body() createSeatDto: CreateSeatDto,
   ): Promise<{ message: string; res: any }> {
     const seat = await this.seatService.createSeat(createSeatDto);
+    return { message: 'Create successfully!', res: seat };
+  }
+  @Post('/many')
+  async createManySeats(
+    @Body() createManySeatsDto: CreateManySeatsDto,
+  ): Promise<{ message: string; res: any }> {
+    const seat = await this.seatService.createManySeats(createManySeatsDto);
     return { message: 'Create successfully!', res: seat };
   }
 }
--- src/api/v1/seat/seat.service.ts
+++ src/api/v1/seat/seat.service.ts
@@ -2,6 +2,8 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSeatDto } from './dto/create-seat.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Seat } from '@prisma/client';
+import { CreateManySeatsDto } from './dto/create-many-seats.dto';
+import { generateSeatNames } from 'src/api/util/createSeatNames.util';
 
 @Injectable()
 export class SeatService {
@@ -17,6 +19,26 @@ export class SeatService {
         data: createSeatDto,
       });
       return seat;
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+  async createManySeats(creatManySeatsDto: CreateManySeatsDto) {
+    try {
+      const { rows, seatsPerRow, seatTypeId, startChar } = creatManySeatsDto;
+      const seatNames = generateSeatNames(rows, seatsPerRow, startChar);
+      const seats = seatNames.map((name) => ({ name, seatTypeId }));
+      const seatType = this.prisma.seatType.findUnique({
+        where: {
+          id: seatTypeId,
+        },
+      });
+      if (!seatType)
+        throw new HttpException('Seat Type Not Found!', HttpStatus.BAD_REQUEST);
+      return await this.prisma.seat.createMany({
+        data: seats,
+        // skipDuplicates: true, // Optional: skip duplicate entries
+      });
     } catch (error) {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
